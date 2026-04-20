4daf63a240ff67152c44d3131da53b268e155031
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-04-20 14:32:24 +0900

    00_04

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <sdkddkver.h>
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
 #pragma warning(pop)
 
 namespace {
@@ -15,6 +17,45 @@ constexpr int32_t kClientHeight = 720;
 }
 
 LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+
+// string->wstring
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return {};
+	}
+
+	const int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+	if (sizeNeeded == 0) {
+		return {};
+	}
+
+	std::wstring result(static_cast<size_t>(sizeNeeded), L'\0');
+	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, result.data(), sizeNeeded);
+	result.pop_back();
+	return result;
+}
+
+// wstring->string
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return {};
+	}
+
+	const int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	if (sizeNeeded == 0) {
+		return {};
+	}
+
+	std::string result(static_cast<size_t>(sizeNeeded), '\0');
+	WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, result.data(), sizeNeeded, nullptr, nullptr);
+	result.pop_back();
+	return result;
+}
+
+// 出力ウィンドウに文字を出す
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
 
 // Windowsアプリケーションのエントリポイント
 int WINAPI WinMain(_In_ HINSTANCE instanceHandle, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -34,6 +75,21 @@ int WINAPI WinMain(_In_ HINSTANCE instanceHandle, _In_opt_ HINSTANCE, _In_ LPSTR
 	if (RegisterClass(&windowClass) == 0) {
 		return 1;
 	}
+	Log("window class registered\n");
+
+	//文字列を格納する
+	std::string str0{ "STRING!!!" };
+	Log(str0 + "\n");
+
+	// 整数を文字列にする
+	std::string str1{ std::to_string(10) };
+	Log(str1 + "\n");
+
+	// wstringをstringに変換してログに出す
+	Log(ConvertString(std::wstring{ L"WSTRING!!!\n" }));
+
+	// formatを使ってログに出す
+	Log(std::format("client size: {} x {}\n", kClientWidth, kClientHeight));
 
 	// クライアント領域のサイズ
 	RECT windowRect{0, 0, kClientWidth, kClientHeight};
@@ -59,6 +115,7 @@ int WINAPI WinMain(_In_ HINSTANCE instanceHandle, _In_opt_ HINSTANCE, _In_ LPSTR
 	if (windowHandle == nullptr) {
 		return 1;
 	}
+	Log("window created\n");
 
 	// ウィンドウを表示する
 	ShowWindow(windowHandle, SW_SHOW);
